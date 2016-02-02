b8066d7124e3fe9fab837385854b2715a2a771d9
Joshua <tantohyung@gmail.com>
2016-02-02 21:27:10 +0800

    add procfile and edit port variable for heroku

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ var mongo = require('mongodb').MongoClient;
 var routes = require('./app/routes/index.js');
 
 var app = express();
-var port = 3000;
+var port = process.env.PORT || 8080;
 var dbUrl = 'mongodb://localhost:27017/jt-timestamp';
 
 mongo.connect(dbUrl, function(err, db){
@@ -17,34 +17,8 @@ mongo.connect(dbUrl, function(err, db){
 
 	routes(app, db);
 
-/*	app.route('/').get(function(req, res){
-		var date = db.collection('date');
-		var dateProjection = {'_id': false};
-		var today = Math.floor(Date.now()/1000);
-		date.findOne({}, dateProjection, function(err, result){
-			if (err) {
-				throw err;
-			}
-			if (result) {
-				res.json(result);
-			} else {
-				date.insert({'date': today}, function(err){
-					if (err) {
-						throw err;
-					} 
-					date.findOne({}, dateProjection, function(err, doc){
-						if (err){
-							throw err;
-						}
-						res.json(doc);
-					});
-				});
-			}
-		});
-	});
-*/
 	app.listen(port, function(){
-		console.log('Listening to port 3000');
+		console.log('Listening to port' + port);
 	});
 });
 
