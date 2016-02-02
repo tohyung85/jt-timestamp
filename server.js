49eebdf0c4dfd7be429664dd4ec1676f5c991fa5
Joshua <tantohyung@gmail.com>
2016-02-02 21:55:21 +0800

    add mongo add-on url and correct Procfile to start with server.js

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ var routes = require('./app/routes/index.js');
 
 var app = express();
 var port = 3000;
-var dbUrl = 'mongodb://heroku_ll42fnqj:ffqjc4h0rsk9o07hcvthdhb05p@ds055515.mongolab.com:55515/heroku_ll42fnqj';
+var dbUrl = 'mongodb://heroku_0scnvt0k:kfhhiodtcl8pa77l9nau9b5c1v@ds055535.mongolab.com:55535/heroku_0scnvt0k';
 
 mongo.connect(dbUrl, function(err, db){
 	if (err) {
