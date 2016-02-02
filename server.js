3053d5c753bc544c474ff47e872e8242a469ddce
Joshua <tantohyung@gmail.com>
2016-02-02 21:57:44 +0800

    edit port number

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ var mongo = require('mongodb').MongoClient;
 var routes = require('./app/routes/index.js');
 
 var app = express();
-var port = 3000;
+var port = process.env.PORT || 8080;
 var dbUrl = 'mongodb://heroku_0scnvt0k:kfhhiodtcl8pa77l9nau9b5c1v@ds055535.mongolab.com:55535/heroku_0scnvt0k';
 
 mongo.connect(dbUrl, function(err, db){
